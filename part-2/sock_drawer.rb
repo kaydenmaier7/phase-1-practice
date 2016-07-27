2f191a4431e2a45ddd3e656b92c033760b873aeb
Youna Yang <email": null>
2016-07-27 10:04:40 -0700

    first pass at supply_match_for & supply_one_pair_of_socks

--- part-2/sock_drawer.rb
+++ part-2/sock_drawer.rb
@@ -1,8 +1,22 @@
+require_relative 'sock_matcher'
 class SockDrawer
   attr_reader :socks, :matcher
 
   def initialize(args = {})
     @socks = args.fetch(:socks) { Array.new }
     @matcher = args.fetch(:matcher)
+  end
+
+  def supply_match_for
+    # when it has a matching sock returns a matching sock
+    if @socks.match? == true
+      @socks.shift
+    end
+    nil
+  end
+
+  def supply_one_pair_of_socks
+    #produce an arbitrary pair of matched socks, return that pair, and ensure the two socks of the pair are removed from the collection
+    socks.rand(1..4).delete
   end
 end
