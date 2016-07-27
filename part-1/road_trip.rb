7b0bf27a53fd57b9933854186e5bb6f2fe48911c
Youna Yang <email": null>
2016-07-27 10:05:28 -0700

    pass at segments, total_miles, average_miles_per_segment

--- part-1/road_trip.rb
+++ part-1/road_trip.rb
@@ -1,2 +1,25 @@
 class RoadTrip
+
+  attr_accessor :name, :segments, :new_segment
+
+  def initialize(name)
+    @name = 'Trip to SF'
+    @segments = []
+    @new_segment = Segment.new
+  end
+
+  def segments(new_segment)
+    @segments << @new_segment
+  end
+
+  def total_miles
+    @segments.each do |x|
+      x.reduce(:+)
+    end
+  end
+
+  def average_miles_per_segment
+    total_miles / @segments.length
+  end
 end
+
