34fbba6f068dd122b0a53518a0d3f1d50e5191a5
Kayden Williams <email": null>
2016-08-11 18:08:40 -0700

    Add methods

--- part-2/sock_drawer.rb
+++ part-2/sock_drawer.rb
@@ -5,4 +5,29 @@ class SockDrawer
     @socks = args.fetch(:socks) { Array.new }
     @matcher = args.fetch(:matcher)
   end
+
+  def supply_match_for(sock_to_match)
+    @socks.each do |sock|
+      if @matcher.match?(sock, sock_to_match)
+        return @socks.delete(sock)
+      end
+    end
+      return nil
+  end
+
+  def supply_one_pair_of_socks
+    # matched_socks = @socks.select {|sock| sock.color == "white"}
+    pairs = []
+
+  end
+
 end
+
+
+
+
+
+
+
+
+
